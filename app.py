4cc71ab5c60458404c85b962017f86638a520ac2
tavishigupta <tavishi.g7@gmail.com>
2018-07-31 23:12:51 -0700

    all inputs in correct format

--- app.py
+++ app.py
@@ -3,18 +3,9 @@ from flask import Flask, request, render_template
 # create flask app
 app = Flask(__name__)
 
-# removes all new lines for correct input for model
+# removes all new lines for correct input to trained model
 def cleanAgenda(agenda):
     return agenda.replace('\n', ' ')
-
-# builds array of items with same key values
-# def arrayBuilder(result, key):
-#     list = []
-    
-#     for key in result.values():
-#         list.append[result[key]]
-
-#     return list
 
 # return homepage for user input
 @app.route("/")
@@ -23,16 +14,21 @@ def main():
 
 # return result page with prediction
 @app.route('/getprediction', methods=['POST','GET'])
-def get_delay():
+def get_prediction():
     if request.method == 'POST':
+        # instantiate all venue and audience inputs
+        offices = stores = schools = premises = otherv = 0
+        under5 = six = twelve = eighteen = over23 = industry = educators = government = othera = 0
+
         # extract all values from result
         result = request.form
         date = result['date']
         agenda = cleanAgenda(result['agenda'])
         length = result['length']
-        venue = list(item['venue'] for item in result)
-        #venue = arrayBuilder(result, 'venue')
-        audience = result['audience']
+
+        # extract list of all venues and audiences
+        venue = request.form.getlist('venue')
+        audience = request.form.getlist('audience')
 
         # validate correct format of each value
         print(result)
@@ -41,6 +37,53 @@ def get_delay():
         print(length)
         print(venue)
         print(audience)
+
+        # 
+        if 'offices' in venue:
+            offices = 1
+        if 'stores' in venue:
+            stores = 1
+        if 'schools' in venue:
+            schools = 1
+        if 'premises' in venue:
+            premises = 1
+        if 'other' in venue:
+            otherv = 1
+
+        if 'under5' in audience:
+            under5 = 1
+        if '6to11' in audience:
+            six = 1
+        if '12to17' in audience:
+            twelve = 1
+        if '18to22' in audience:
+            eighteen = 1
+        if '23Over' in audience:
+            over23 = 1
+        if 'industry' in audience:
+            industry = 1
+        if 'educators' in audience:
+            educators = 1
+        if 'government' in audience:
+            government = 1
+        if 'other' in audience:
+            othera = 1
+
+        # validate correct value for venue and audience input
+        print(offices)
+        print(stores)
+        print(schools)
+        print(premises)
+        print(otherv)
+        print(under5)
+        print(six)
+        print(twelve)
+        print(eighteen)
+        print(over23)
+        print(industry)
+        print(educators)
+        print(government)
+        print(othera)
 
         # pkl_file = open('cat', 'rb')
         # index_dict = pickle.load(pkl_file)
@@ -54,5 +97,5 @@ def get_delay():
 
 # check if the executed file is the main program and run the app
 if __name__ == "__main__":
-    app.debug = True # print tracebacks to console
+    app.debug = True # print traceback to console
     app.run()
