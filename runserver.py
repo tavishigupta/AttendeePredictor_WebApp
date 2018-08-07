b2822922a26edc70fd14c7ff4e08b6aa9f647fd5
tavishigupta <tavishi.g7@gmail.com>
2018-08-07 10:53:04 -0700

    updated structure

--- runserver.py
+++ runserver.py
@@ -12,7 +12,7 @@ wsgi_app = app.wsgi_app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5000'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 5000
+        PORT = 5555
     app.run(HOST, PORT, debug=True) # print traceback to console
